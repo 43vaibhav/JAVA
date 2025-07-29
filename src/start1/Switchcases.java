ccca62d87bd77f8b248c5f320f3001a09ab77324
VAIBHVA ADHIKARI <155263128+43vaibhav@users.noreply.github.com>
2025-07-29 23:00:28 +0530

    Add files via upload

---
+++ src/start1/Switchcases.java
@@ -0,0 +1,15 @@
+package start1;
+
+public class Switchcases {
+    public static void  main(String args[]){
+        String s = "mango";
+        String g = "orange";
+        if(s.equals(g)){
+            System.out.println(true);
+
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
