de083f0879592a7e7970345c1af312651883e5a1
VAIBHVA ADHIKARI <155263128+43vaibhav@users.noreply.github.com>
2025-07-30 23:09:35 +0530

    Add files via upload

---
+++ src/leetmed/SumThreePower.java
@@ -0,0 +1,12 @@
+package leetmed;
+
+public class SumThreePower {
+    public static void main(String args[]) {
+        int n=12;
+        int ans = n % 3 ;
+        if(ans ==0 || ans == 1) {
+        
+        }        
+
+    }
+}
