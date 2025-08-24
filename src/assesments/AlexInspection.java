f47370ff573e71e7df7283a83fea869d3e54fe13
VAIBHVA ADHIKARI <155263128+43vaibhav@users.noreply.github.com>
2025-08-25 00:14:35 +0530

    Update AlexInspection.java

--- src/assesments/AlexInspection.java
+++ src/assesments/AlexInspection.java
@@ -26,20 +26,7 @@ Print the total results at the end.
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int inspections[][] = new int [10][2];
-        for(int i  = 0 ; i < 10;i++){
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-            inspections[i][0] = r;
-            inspections[i][1] = c;
-        }
-            
-        
-        for(int  i = 0 ; i < 10; i++){
-            for(int j = 0; j< 2;j++){
-                System.out.print(inspections[i][j]);
-            }
-            System.out.println();
-        }
+
         char grid[][] = new char[5][5];
         for(int  i =0;i<5;i++){
             for(int j  =0;j< 5;j++){
@@ -50,6 +37,45 @@ Print the total results at the end.
         grid[2][3] = 'C';
         grid[4][4] = 'C';
         
-                
+
+        int chargingFound = 0 ;
+        int emptyFound = 0;
+
+        for(int i  = 0 ; i < 10;i++){
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            inspections[i][0] = r;
+            inspections[i][1] = c;
+
+        }
+        for(int k = 0;k<10;k++){
+            int r = inspections[k][0];
+            int c = inspections[k][1];
+            
+            if(grid[r][c] == 'C'){
+                System.out.println("Charging Station Found!");
+                chargingFound++;
+            }
+            else{
+                System.out.println("Not Found!");
+                emptyFound++;
+            }
+            grid[r][c] = 'X';
+
+            System.out.println();
+            System.out.println();
+            System.out.print("Updated Grid");
+            for(int j = 0;j<5;j++){
+                for(int s = 0;s<5;s++){
+                    System.out.print(grid[j][s]+ " ");
+                }
+                System.out.println();
+            }  
+        }
+        System.out.println("Total Charging Station: " + chargingFound);
+        System.out.println("No Charging Station: " + emptyFound);
+        sc.close();
     }
 }
+    
+
