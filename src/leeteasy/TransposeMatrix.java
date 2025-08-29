7503d902933a64ef9978578b0c03d12f7d397f82
vaibhav adhikari <vaaad4590@gmail.com>
2025-08-30 01:22:51 +0530

    modified file

--- src/leeteasy/TransposeMatrix.java
+++ src/leeteasy/TransposeMatrix.java
@@ -14,33 +14,29 @@ public class TransposeMatrix {
             }
         }
         sc.close();
-        int ans[][]= TransposeMatrix(matrix,m,n);
+        int ans[][] = TransposeMatrix(matrix,m,n);
         PrintMatrix(ans,m,n);
         
 
     }
     static void PrintMatrix(int arr[][],int m, int n){
-        for(int i = 0 ; i< m;i++){
-            for(int j = 0 ; j < n;j++){
+        
+        for(int i = 0 ; i< n;i++){
+            for(int j = 0; j < m;j++){
+                
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
-    static int[][] TransposeMatrix(int matrix[][],int m, int n){
+    static int[][] TransposeMatrix(int arr[][],int m, int n){
+        int ans[][] = new int[n][m];
         for(int i = 0 ; i< n;i++){
-            for(int j = i;j<m;j++){
-                Swap(matrix,i,j);
+            for(int j = 0 ; j < m;j++){
+                ans[i][j]  = arr[j][i];
             }
+            System.out.println();
         }
-        return matrix;
-        
-    }
-    static int Swap(int matrix[][] , int i, int j){
-        int temp = matrix[j][i];
-        matrix[j][i] = matrix[i][j];
-        matrix[i][j] = temp;
-        return temp;
-
+        return ans;
     }
 }
