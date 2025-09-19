5c89e4b9d4b187ace4a6b74bed35e65a219d52c3
Vaibhav Adhikari <vaad4590@gmail.com>
2025-09-19 23:25:50 +0530

    COMMIT FILE

--- src/recursion/Sum.java
+++ src/recursion/Sum.java
@@ -1,22 +1,32 @@
 package recursion;
 import java.util.Scanner;
-public class Counting {
+public class Sum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        System.out.print("SUM OF  "+ n+ " INTGERS IS : ");
-        System.out.print(Sum(n));
+        System.out.println("ENTER THE VALUE OF N : ");
+        int n = sc.nextInt();
+        System.out.println("SUMMTION USING PARAMETERS: " );
+        System.out.print(SumP(0,n));
+        System.out.println();
+        System.out.println("SUMMTION USING FUNCTIONS: ");
+        System.out.println(SumF(n));
         sc.close();
     }
-    static int Sum(int n){
+    static int SumF(int n){
         /* PRINT THE SUM OF FIRST N INTEGERS */
         int sum= 0;
         if(n == 0){
             return sum;
         }
-        sum = n + Sum(n-1);
+        sum = n + SumF(n-1);
         return sum;
-
-
+    }
+    static int SumP(int i,int n){
+        /*PRINTING SUM USINF PARAMETER I WHERE AFTER EAXCH CALL I BECOMES PLUS 1 */
+        if(i==n){
+            return i;
+        }
+        int sum = i + SumP(i+1,n);
+        return sum; 
     }
 }
