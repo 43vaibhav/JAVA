22d4b5d7529c96af6f1ae1f7c0ed17d7e94281a9
Vaibhav Adhikari <vaad4590@gmail.com>
2025-09-24 14:38:14 +0530

    COMMIT

--- src/leetmed/versionCompare.java
+++ src/leetmed/versionCompare.java
@@ -6,6 +6,47 @@ import java.util.List;
 
 
 public class versionCompare {
+    /*165. Compare Version Numbers
+Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+
+To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+
+Return the following:
+
+If version1 < version2, return -1.
+If version1 > version2, return 1.
+Otherwise, return 0.
+ 
+
+Example 1:
+
+Input: version1 = "1.2", version2 = "1.10"
+
+Output: -1
+
+Explanation:
+
+version1's second revision is "2" and version2's second revision is "10": 2 < 10, so version1 < version2.
+
+Example 2:
+
+Input: version1 = "1.01", version2 = "1.001"
+
+Output: 0
+
+Explanation:
+
+Ignoring leading zeroes, both "01" and "001" represent the same integer "1".
+
+Example 3:
+
+Input: version1 = "1.0", version2 = "1.0.0.0"
+
+Output: 0
+
+Explanation:
+
+version1 has less revisions, which means every missing revision are treated as "0 */
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("VERSION1 : ");
@@ -16,6 +57,8 @@ public class versionCompare {
         System.out.println(usingTokens(v1,v2));
         System.out.println("USING SPLIT: ");
         System.out.println(usingSplit(v1,v2));
+        System.out.println("USING LOOPS:");
+        System.out.println(usingLoops(v1,v2));
         sc.close();
 
     }
@@ -58,5 +101,30 @@ public class versionCompare {
             if(num2 > num1) return -1;
         }
         return 0;
+    }
+    static int usingLoops(String v1, String v2){
+        int n1 = v1.length();
+        int n2 = v2.length();
+        int i=0;int j=0;
+
+        while(i < n1 || j < n2){
+            int num1 =0;
+            int num2 =0;
+            while(i < n1 && v1.charAt(i) != '.'){
+                num1 = num1 * 10 + (v1.charAt(i) - '0');
+                i++;
+            }
+
+            while(j < n2 && v2.charAt(j) != '.'){
+                num2 = num2 * 10 + (v2.charAt(j)  - '0');
+                j++;
+            }
+            if(num1 > num2) return 1;
+            if(num2 > num1) return -1;
+            i++;
+            j++;
+        }
+        return 0;
+
     }
 }
