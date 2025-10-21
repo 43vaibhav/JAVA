3142f0045ecdc9a3b9888c4595ad32eaaf869f15
Vaibhav Adhikari <vaad4590@gmail.com>
2025-10-21 21:26:51 +0530

    Commit Files

--- src/recursion/arrayCheckSort.java
+++ src/recursion/arrayCheckSort.java
@@ -1,3 +1,14 @@
+package recursion;
 public class arrayCheckSort {
-
-}
+    public static void main(String args[]){
+        int arr[] = {2,4,4,5,6};
+        boolean flag = false;
+        boolean ans = checkSort(arr,0,flag);
+    }
+    static boolean checkSort(int  arr[], int i, boolean flag){
+        if(i == 5){
+            return flag;
+        }
+        if()
+    }
+} 
