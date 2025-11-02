3a9caef1252cabcfdecf0d952e3debb8c4c8ec24
Vaibhav Adhikari <vaad4590@gmail.com>
2025-11-02 23:19:07 +0530

    Commit

---
+++ src/leeteasy/commonPrefix.java
@@ -0,0 +1,21 @@
+package leeteasy;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class commonPrefix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] strs = new String[n];
+        for(int i = 0;i<n;i++){
+            strs[i] = sc.nextLine();
+        } 
+        String result = longestCommonPrefix(strs);
+        System.out.println(result);
+    }
+    static String longestCommonPrefix(String[] strs){
+        Arrays.sort(strs);
+        return strs[0];
+    }
+}
